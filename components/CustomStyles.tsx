0fe7c8eaf6c4bf7b9bc5cb118ac025918112634d
83keith <keith_83@icloud.com>
2023-08-19 19:59:37 +0100

    added style locally for ssl

---
+++ components/CustomStyles.tsx
@@ -0,0 +1,10 @@
+export function RemoteStyles() {
+  return (
+    <>
+      <link href="/css/style.css" rel="stylesheet" />
+      <link href="/css/css/fontawesome.min.css" rel="stylesheet" />
+      <link href="/css/css/brands.min.css" rel="stylesheet" />
+      <link href="/css/css/solid.min.css" rel="stylesheet" />
+    </>
+  );
+}
